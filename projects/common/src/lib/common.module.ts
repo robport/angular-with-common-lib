b10ca0ec951ebafb4afcdcab6a797abb06339b41
robport <26696668+robport@users.noreply.github.com>
2021-01-09 11:01:28 +0000

    Initial Commit

---
+++ projects/common/src/lib/common.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonComponent } from "./common.component";
+import { TestComponent } from './test/test.component';
+
+
+@NgModule({
+  declarations: [CommonComponent, TestComponent],
+  imports: [],
+  exports: [CommonComponent, TestComponent]
+})
+export class CommonModule {
+}
