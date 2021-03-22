b5743da977ec6ed864b1883a3bec43b248fa3994
robport <26696668+robport@users.noreply.github.com>
2021-03-22 10:15:42 +0000

    Company Example

--- projects/my-second-app/src/app/app.component.ts
+++ projects/my-second-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Company } from "common";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,9 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "my-second-app";
+  company: Company = {
+    name: "Rob's Co",
+    employees: 20
+  };
+
 }
